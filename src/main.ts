338e5186e2ded2670c80b43a80a2e0e2165c5c9f
ZaYTI <riquart.antonin@gmail.com>
2026-03-29 23:44:14 +0200

    🐛 Fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: false });
   app.setGlobalPrefix("api");
+  app.enableCors({
+    origin: process.env.FRONT_ORIGIN,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
