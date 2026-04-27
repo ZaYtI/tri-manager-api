c4d6cfb953f48504a17cea222640ee90c7d6bfb0
ZaYTI <riquart.antonin@gmail.com>
2026-04-27 14:59:26 +0200

    🚧 Work on org guard

--- src/organization/organization.controller.ts
+++ src/organization/organization.controller.ts
@@ -1,9 +1,8 @@
 import { Body, Controller, Get, Param, Post, UseGuards } from "@nestjs/common";
 import { OrganizationService } from "./organization.service";
 import { CreateOrganizationDto } from "./dto/create-organization.dto";
-import { Roles } from "~/auth/role/decorators/role.decorator";
-import { AuthGuard } from "~/auth/guards/auth.guard";
 import { Paginate, PaginateQuery } from "nestjs-paginate";
+import { AuthGuard, Roles } from "@thallesp/nestjs-better-auth";
 
 @Controller("organizations")
 @UseGuards(AuthGuard)
@@ -11,26 +10,20 @@ export class OrganizationController {
   constructor(private readonly organizationService: OrganizationService) {}
 
   @Get()
-  @Roles("admin")
+  @Roles(["admin"])
   async findAll(@Paginate() query: PaginateQuery) {
     return this.organizationService.findAll(query);
   }
 
   @Post()
-  @Roles("admin")
+  @Roles(["admin"])
   async create(@Body() body: CreateOrganizationDto) {
     return this.organizationService.create(body.name, body.slug, body.ownerId);
   }
 
   @Get(":id")
-  @Roles("admin")
+  @Roles(["admin"])
   async findOne(@Param("id") id: string) {
     return this.organizationService.findOne(id);
-  }
-
-  @Get(":id/members")
-  @Roles("admin")
-  async findMembers(@Param("id") id: string) {
-    return this.organizationService.findMembers(id);
   }
 }
