a45cdc9c3d28fa0cd90143d93d6680141d99715c
ZaYTI <riquart.antonin@gmail.com>
2026-09-08 14:52:37 +0200

    ✨ Add ban reasons and time in database

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -23,6 +23,8 @@ export class UserService {
         "role",
         "emailVerified",
         "image",
+        "banned",
+        "banReason",
         "createdAt",
         "updatedAt",
       ],
